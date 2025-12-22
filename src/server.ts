39bd3017c93037cf31cfcfb79167998f821de48c
Divyansh Bhardwaj <divyanshb22@gmail.com>
2025-12-22 14:07:51 +0530

    add TypeScript and migration configuration files; implement health check endpoint in server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.listen(
-	PORT,
-	() => {
-		console.log(`server started on port: ${PORT}`)
-	}
-);
+app.get('/api/v1/health', (request, response) => {
+	response
+		.status(200)
+		.send({
+			status: 'ok'
+		})
+})
+
+app.listen(PORT, () => {
+	console.log(`server started on port: ${PORT}`)
+});
 
